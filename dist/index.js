141c31d031ace8661721903e9d4da93ee058156f
foooomio <foooomio@gmail.com>
2021-09-08 08:20:24 +0900

    Download latest from CDN

--- dist/index.js
+++ dist/index.js
@@ -3281,126 +3281,6 @@ function _unique(values) {
 
 /***/ }),
 
-/***/ 296:
-/***/ (function(module) {
-
-/* global define */
-(function (root, factory) {
-  /* istanbul ignore next */
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (true) {
-    module.exports = factory();
-  } else {}
-}(this, function () {
-
-  var semver = /^v?(?:\d+)(\.(?:[x*]|\d+)(\.(?:[x*]|\d+)(\.(?:[x*]|\d+))?(?:-[\da-z\-]+(?:\.[\da-z\-]+)*)?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?)?)?$/i;
-
-  function indexOrEnd(str, q) {
-    return str.indexOf(q) === -1 ? str.length : str.indexOf(q);
-  }
-
-  function split(v) {
-    var c = v.replace(/^v/, '').replace(/\+.*$/, '');
-    var patchIndex = indexOrEnd(c, '-');
-    var arr = c.substring(0, patchIndex).split('.');
-    arr.push(c.substring(patchIndex + 1));
-    return arr;
-  }
-
-  function tryParse(v) {
-    return isNaN(Number(v)) ? v : Number(v);
-  }
-
-  function validate(version) {
-    if (typeof version !== 'string') {
-      throw new TypeError('Invalid argument expected string');
-    }
-    if (!semver.test(version)) {
-      throw new Error('Invalid argument not valid semver (\''+version+'\' received)');
-    }
-  }
-
-  function compareVersions(v1, v2) {
-    [v1, v2].forEach(validate);
-
-    var s1 = split(v1);
-    var s2 = split(v2);
-
-    for (var i = 0; i < Math.max(s1.length - 1, s2.length - 1); i++) {
-      var n1 = parseInt(s1[i] || 0, 10);
-      var n2 = parseInt(s2[i] || 0, 10);
-
-      if (n1 > n2) return 1;
-      if (n2 > n1) return -1;
-    }
-
-    var sp1 = s1[s1.length - 1];
-    var sp2 = s2[s2.length - 1];
-
-    if (sp1 && sp2) {
-      var p1 = sp1.split('.').map(tryParse);
-      var p2 = sp2.split('.').map(tryParse);
-
-      for (i = 0; i < Math.max(p1.length, p2.length); i++) {
-        if (p1[i] === undefined || typeof p2[i] === 'string' && typeof p1[i] === 'number') return -1;
-        if (p2[i] === undefined || typeof p1[i] === 'string' && typeof p2[i] === 'number') return 1;
-
-        if (p1[i] > p2[i]) return 1;
-        if (p2[i] > p1[i]) return -1;
-      }
-    } else if (sp1 || sp2) {
-      return sp1 ? -1 : 1;
-    }
-
-    return 0;
-  };
-
-  var allowedOperators = [
-    '>',
-    '>=',
-    '=',
-    '<',
-    '<='
-  ];
-
-  var operatorResMap = {
-    '>': [1],
-    '>=': [0, 1],
-    '=': [0],
-    '<=': [-1, 0],
-    '<': [-1]
-  };
-
-  function validateOperator(op) {
-    if (typeof op !== 'string') {
-      throw new TypeError('Invalid operator type, expected string but got ' + typeof op);
-    }
-    if (allowedOperators.indexOf(op) === -1) {
-      throw new TypeError('Invalid operator, expected one of ' + allowedOperators.join('|'));
-    }
-  }
-
-  compareVersions.validate = function(version) {
-    return typeof version === 'string' && semver.test(version);
-  }
-
-  compareVersions.compare = function (v1, v2, operator) {
-    // Validate operator
-    validateOperator(operator);
-
-    // since result of compareVersions can only be -1 or 0 or 1
-    // a simple map can be used to replace switch
-    var res = compareVersions(v1, v2);
-    return operatorResMap[operator].indexOf(res) > -1;
-  }
-
-  return compareVersions;
-}));
-
-
-/***/ }),
-
 /***/ 371:
 /***/ ((module) => {
 
@@ -11719,9 +11599,6 @@ function fixResponseChunkedTransferBadEnding(request, errorCallback) {
 	});
 }
 
-// EXTERNAL MODULE: ./node_modules/compare-versions/index.js
-var compare_versions = __nccwpck_require__(296);
-var compare_versions_default = /*#__PURE__*/__nccwpck_require__.n(compare_versions);
 // EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
 var core = __nccwpck_require__(186);
 // EXTERNAL MODULE: ./node_modules/@actions/tool-cache/lib/tool-cache.js
@@ -11734,29 +11611,22 @@ var external_path_default = /*#__PURE__*/__nccwpck_require__.n(external_path_);
 
 
 
-
+const CDN_PAGE_URL = 'https://dlcdn.apache.org/jena/binaries/';
 const ARCHIVE_PAGE_URL = 'https://archive.apache.org/dist/jena/binaries/';
 async function getLatestVersion() {
-    const response = await fetch(ARCHIVE_PAGE_URL);
+    const response = await fetch(CDN_PAGE_URL);
     if (!response.ok) {
-        throw new Error('Could not get the archive page.');
+        throw new Error(`Could not get ${CDN_PAGE_URL}: ${response.status} ${response.statusText}`);
     }
     const html = await response.text();
-    const versions = [];
-    const regexp = /href="apache-jena-(\d+\.\d+\.\d+)\.tar\.gz"/g;
-    for (const [, version] of html.matchAll(regexp)) {
-        if (version) {
-            versions.push(version);
-        }
-    }
-    const [latest] = versions.sort((a, b) => compare_versions_default()(b, a));
-    if (!latest) {
+    const [, version] = html.match(/href="apache-jena-(\d+\.\d+\.\d+)\.tar\.gz"/) || [];
+    if (!version) {
         throw new Error('Could not get the latest version.');
     }
-    return latest;
+    return version;
 }
-async function installJena(version) {
-    const downloadUrl = `${ARCHIVE_PAGE_URL}apache-jena-${version}.tar.gz`;
+async function installJena(version, baseUrl) {
+    const downloadUrl = `${baseUrl}apache-jena-${version}.tar.gz`;
     const archivePath = await tool_cache.downloadTool(downloadUrl);
     const flags = ['xz', '--strip=1'];
     const extractedPath = await tool_cache.extractTar(archivePath, undefined, flags);
@@ -11765,12 +11635,13 @@ async function installJena(version) {
 }
 async function run() {
     let version = core.getInput('jena-version');
-    if (!version || version === 'latest') {
+    const latest = !version || version === 'latest';
+    if (latest) {
         version = await getLatestVersion();
     }
     let jenaPath = tool_cache.find('jena', version);
     if (!jenaPath) {
-        jenaPath = await installJena(version);
+        jenaPath = await installJena(version, latest ? CDN_PAGE_URL : ARCHIVE_PAGE_URL);
     }
     core.exportVariable('JENA_HOME', jenaPath);
     core.addPath(external_path_default().join(jenaPath, 'bin'));

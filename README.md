f060a402d581a350947673feb7885b3f5ffd3148
foooomio <foooomio@gmail.com>
2026-08-12 20:52:50 +0900

    Migrate to Rolldown

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ GitHub Action to setup Apache Jena
 ```yml
 runs-on: ubuntu-latest
 steps:
-  - uses: actions/checkout@v6
+  - uses: actions/checkout@v7
   - uses: actions/setup-java@v5
     with:
       distribution: temurin
       java-version: 21
   - uses: foooomio/setup-jena@v4
     with:
-      # The version of Apache Jena to be installed
+      # Optional. The version of Apache Jena to be installed.
       # Examples: 5.x, 5.6.0, latest
       # Default: latest
       jena-version: latest
--- test/jena-version.test.ts
+++ test/jena-version.test.ts
@@ -1,20 +1,18 @@
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
-import { getLatest, getSatisfied } from '../src/jena-version';
+import assert from "node:assert/strict";
+import test from "node:test";
+import { getLatest, getSatisfied } from "../src/jena-version.ts";
 
-test('latest', async () => {
+test("latest", async () => {
   const info = await getLatest();
   assert.ok(info.version);
 });
 
-test('range', async () => {
-  const info = await getSatisfied('3.x');
-  assert.is(info.version, '3.17.0');
+test("range", async () => {
+  const info = await getSatisfied("3.x");
+  assert.equal(info.version, "3.17.0");
 });
 
-test('2.7.0-incubating', async () => {
-  const info = await getSatisfied('2.7.0');
-  assert.is(info.version, '2.7.0-incubating');
+test("2.7.0-incubating", async () => {
+  const info = await getSatisfied("2.7.0");
+  assert.equal(info.version, "2.7.0-incubating");
 });
-
-test.run();
